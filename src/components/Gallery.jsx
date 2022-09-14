d3fb0d35103bef6275a6425c651c1062890e3444
JoakimIEngstrom <joakim.engstrom@utb.ecutbildning.se>
2022-09-14 15:18:18 +0200

    import images and put into gallery

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -2,8 +2,14 @@ import React from "react";
 
 const Gallery = () => {
   return (
-    <div className="Gallery">
-      <h1>This is Gallery</h1>     
+    <div>
+        <h1>This is Gallery</h1>    
+    <div className="Gallery">      
+      <img className="galleryPic" src="./images/WHFB1.jpg" alt="pic1" />
+      <img className="galleryPic" src="./images/WHFB2.jpg" alt="pic2" />  
+      <img className="galleryPic" src="./images/WHFB3.jpg" alt="pic3" />
+      <img className="galleryPic" src="./images/WHFB4.jpg" alt="pic4" />         
+    </div>
     </div>
   );
 };
